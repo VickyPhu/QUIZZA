2948b19971c2df89fc1655c03cdf532042997083
VickyPhu <victoriaphu@gmail.com>
2025-02-23 23:49:47 +0100

    set up routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import QuizPage from "./pages/QuizPage.tsx";
 import StartPage from "./pages/StartPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <StartPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />}></Route>
+        <Route path="/quiz" element={<QuizPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
