a9b6225200013f13a026c0c50da6499ac974abca
VickyPhu <victoriaphu@gmail.com>
2025-02-25 00:45:02 +0100

    set up QuizPage

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,3 +1,11 @@
+import QuestionCard from "../components/QuestionCard";
+import GlobalStyle from "../styles/GlobalStyles";
+
 export default function QuizPage() {
-    return <h1>Quiz</h1>
+  return (
+    <>
+      <GlobalStyle />
+      <QuestionCard />
+    </>
+  );
 }
